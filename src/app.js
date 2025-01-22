f3aca6e012afa1f5e46af45bfd5abf57d0d8c1cc
mrsbrightside001 <155654171+mrsbrightside001@users.noreply.github.com>
2025-01-22 11:28:28 +0100

    Added search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#weather-app-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "tb561b9af40b32focb07736aa7b1c96e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("New York");
