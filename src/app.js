71ed8525a23d23ee706744ee6b5d256bbc96aa86
mrsbrightside001 <155654171+mrsbrightside001@users.noreply.github.com>
2025-01-23 17:25:00 +0100

    Add boilerplate for API integration to js script

--- src/app.js
+++ src/app.js
@@ -55,7 +55,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+        <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤</div>
+            <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature">
+                    <strong>17°C</strong>
+                </div>
+                <div class="weather-forecast-temperature">9°C</div>
+            </div>
+        </div>
+    `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("New York");
+displayForecast();
